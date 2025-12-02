0ca2e0c7dfe28066b09a73891c0b8b22f13f3956
zKazemeini125 <kazemeiniz125@gmail.com>
2025-12-02 15:08:12 +0330

    set nuxt config 1404/09/11

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+   extends: [
+    '../front-core',
+    '../front-modules/auth'
+  ],
 })
+
