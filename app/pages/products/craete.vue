fcabc1f331b917fa87eacd27bce4d3d8168e70f5
zKazemeini125 <kazemeiniz125@gmaol.com>
2025-12-13 22:46:04 +0330

    product

---
+++ app/pages/products/craete.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    const form=reactive({
+        id:0,
+        title:'',
+    })
+</script>
+<template>
+    <v-card>
+        <v-text-field></v-text-field>
+    </v-card>
+</template>
+<style>
+
+</style>
