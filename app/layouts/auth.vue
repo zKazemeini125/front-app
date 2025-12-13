0a0e86ba3a195e28df013eabe055c62ac01d1b53
zKazemeini125 <kazemeiniz125@gmaol.com>
2025-12-13 22:21:29 +0330

    add layouts

---
+++ app/layouts/auth.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-container
+    fluid
+    class="w-100 h-screen d-flex flex-row align-center justify-center"
+  >
+    <slot></slot>
+  </v-container>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const router = useRouter();
+</script>
+
+<template>
+  <v-layout>
+    <v-app>
+       <v-navigation-drawer>
+        <v-list>
+          <v-list-item>Shop</v-list-item>
+          <v-list-item>Products</v-list-item>
+          <v-list-item>Blogs</v-list-item>
+          <v-list-item> Settings </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar density="compact" title="username">
+        <v-btn
+          color="blue"
+          class="text-none"
+          append-icon="mdi-logout"
+          @click="
+            () => {
+              router.push({ name: 'auth-signIn' });
+            }
+          "
+          >Logout</v-btn
+        >
+      </v-app-bar>
+     <v-main class="d-flex align-center justify-center">
+      <slot></slot>
+    </v-main>
+    </v-app>
+    
+  </v-layout>
+  <c> </c>
+</template>
+
+<style scoped></style>
